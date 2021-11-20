275d4728b35c1940c51685dba9aadaa6806181b8
CristoGuerrero <92631968+CristoGuerrero@users.noreply.github.com>
2021-11-20 14:34:12 +0100

    Update POKEDEX.js

--- POKEDEX.js
+++ POKEDEX.js
@@ -1,24 +1,36 @@
-function getRandomInt(min, max){
-    return Math.floor(Math.random() * (max - min)) + min;
-}
-console.log()
-document.addEventListener('DOMContentLoaded', () => {
-    const random = getRandomInt(1,151)
-    fetchApi(random)  
-})
+const pokemonContainer = document.getElementById('.pokemonContainer')
 
-const fetchApi = async(API) => {
-    try {
-        const respuesta = await fetch(`https://pokeapi.co/api/v2/pokemon/${API}`)
-        const stats = await respuesta.json()
-        console.log(stats)
-        pintarTemplate(stats)
-
-    } catch (error) {
-        console.log(error)
-    }
+function fetchPokemon(id) {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+    .then((res) => res.json())
+    .then((data) => {createPokemon(data)
+    })
 }
+fetchPokemon(2);
 
-const pintarTemplate = (pokemon) => {
-    const template = document.getElementById()
+function createPokemon(pokemon){
+    const card = document.createElement('div')
+    card.classList.add('pokemon-block');
+
+    const spriteContainer = document.createElement('div');
+    spriteContainer.classList.add('img-container');
+
+    const sprite = document.createElement('img');
+    sprite.src = pokemon.sprites.front_default
+
+    spriteContainer.appendChild(sprite);
+
+    const number = document.createElement('p');
+    number.textContent = `#${pokemon.id.toString().padStart(3,0)}`;
+
+    const name = document.createElement('p');
+    name.classList.add('name');
+    name.textContent = pokemon.name
+
+    card.appendChild(spriteContainer);
+    card.appendChild(number);
+    card.appendChild(name);
+
+    pokemonContainer.appendChild(card);
 }
+fetchPokemon(2);
